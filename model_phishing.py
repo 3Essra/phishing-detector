53d9e914cdfe32343ca2af48d287b91c74b350be
Essra3 <431220422@students.arabeast.edu.sa>
2025-04-26 19:56:24 +0300

    تحسين شكل الموقع مع رفع الدقة

--- model_phishing.py
+++ model_phishing.py
@@ -14,7 +14,15 @@ y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # تهيئة الvectorizer
-vectorizer = TfidfVectorizer(ngram_range=(1, 2), max_df=0.9, min_df=5, stop_words='english', max_features=50)
+vectorizer = TfidfVectorizer(
+    ngram_range=(1, 2),
+    max_df=0.95,
+    min_df=2,
+    stop_words='english',
+    max_features=1000
+)
+
+
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
